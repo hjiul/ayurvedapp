a837ae4ee85b5a0b241da34e7bcd8b5a80a6986a
hjiul <esteban.utopia87@gmail.com>
2020-09-16 07:11:36 -0600

    travis

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,34 @@ module.exports = {
   ],
   devServer: {
     proxy: 'https://script.google.com'
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        "appId": "com.ayurvedapp.app",
+        "publish": "github",
+        "dmg": {
+          "contents": [
+            {
+                "x": 110,
+                "y": 150
+              },
+              {
+                "x": 240,
+                "y": 150,
+                "type": "link",
+                "path": "/Applications"
+              }
+            ]
+          },
+          "appImage": {
+            "license": "LICENSE"
+          },
+          "nsis": {
+            "createDesktopShortcut": "always",
+            "license": "LICENSE"
+          }
+      }
+    }
   }
 }
